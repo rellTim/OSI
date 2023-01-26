24e3175a25a45bdecd3eb28cee11d0d6efc1c187
Tim <book9902@yandex.ru>
2023-01-26 19:36:22 +0300

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    private static final int PORT = 8089;
+    private static final String HOST = "127.0.0.1";
+
+    public static void main(String[] args) {
+        try (var socket = new Socket(HOST, PORT);
+             var out = new PrintWriter(socket.getOutputStream(), true);
+             var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            out.println(out);
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Serves.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.ServerSocket;
+
+public class Serves {
+    private static final int PORT = 8089;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("servers started");
+        try (var serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                try (var clientSocket = serverSocket.accept(); var out = new PrintWriter(clientSocket.getOutputStream(), true); var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.println("New connection accepted. Port: " + clientSocket.getPort());
+                    var name = in.readLine();
+                    out.println("Hi " + name + " your port is " + clientSocket.getPort());
+                }
+            }
+        }
+    }
+}
